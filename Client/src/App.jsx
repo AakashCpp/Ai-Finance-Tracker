eabeae5887f96c5cd039d0aa28781a4c23fa1dd7
rakhi <prasadrakhi20004@gmail.com>
2025-03-28 12:46:27 +0530

    About Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
-import Lnading from './pages/Landing'
+import React, { useState } from "react";
+import Lnading from "./pages/Landing";
+import About from "./pages/About";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Lnading />
+      {/* <Lnading /> */}
+      <About />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
